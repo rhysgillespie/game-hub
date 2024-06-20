561992999e3453e7c4e5570a7262ac28b5375a96
Rhys Gillespie <rhys.gillespie@hotmail.co.uk>
2024-06-20 13:43:01 +0100

    Build a navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+import { Image, HStack, Text } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+
+const NavBar = () => {
+  return (
+    <HStack>
+      <Image src={logo} boxSize="60px" />
+      <Text>NavBar</Text>
+    </HStack>
+  );
+};
+
+export default NavBar;

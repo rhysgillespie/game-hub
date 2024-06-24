61d18d970de3b07ad20a4038bbcbdca414de9408
Rhys Gillespie <rhys.gillespie@hotmail.co.uk>
2024-06-24 15:32:54 +0100

    Sort the games

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -18,8 +18,12 @@ export interface Game {
 const useGames = (
   gameQuery: GameQuery
   ) => 
-useData<Game>('/games', {params: {genres: gameQuery.genre?.id, 
-  parent_platforms: gameQuery.platform?.id
+useData<Game>('/games',
+ {
+  params: {
+    genres: gameQuery.genre?.id, 
+    parent_platforms: gameQuery.platform?.id,
+    ordering: gameQuery.sortOrder
 }}, 
 [gameQuery]);
 
